7363d97626d7371133acb476b3c2600606548b6e
Ronan Hughes <rhughes490@gmail.com>
2020-06-05 21:28:32 +0100

    File structure changed and database schema added

---
+++ db/rigadvisor.sql
@@ -0,0 +1,25 @@
+DROP TABLE flights;
+DROP TABLE oil_workers;
+DROP TABLE rigs;
+
+CREATE TABLE oil_workers (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  contact_num INT
+);
+
+CREATE TABLE rigs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  type VARCHAR(255),
+  distance INT,
+  country VARCHAR(255)
+);
+
+CREATE TABLE flights (
+  id SERIAL PRIMARY KEY,
+  oil_worker_id INT REFERENCES oil_workers(id) ON DELETE CASCADE,
+  rig_id INT REFERENCES rigs(id) ON DELETE CASCADE,
+  status TEXT
+);
