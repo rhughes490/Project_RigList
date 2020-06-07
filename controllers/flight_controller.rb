2c85546b5a6f997d4ac6f434f32f0ea6fe4fdf39
Ronan Hughes <rhughes490@gmail.com>
2020-06-07 15:10:39 +0100

    Added a Flight controller

---
+++ controllers/flight_controller.rb
@@ -0,0 +1,31 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( '../models/flight.rb' )
+require_relative( '../models/oil_worker.rb' )
+require_relative( '../models/rig.rb' )
+require_relative( '../models/action.rb' )
+also_reload( '../models/*' )
+
+get '/flights' do
+  @flights = Flight.all
+  @actions = Action.all
+  erb ( :"flights/index" )
+end
+
+get '/flights/new' do
+  @oil_workers = Oil_worker.all
+  @rigs = Rig.all
+  erb(:"flights/new")
+end
+
+post '/flights' do
+  flight = Flight.new(params)
+  flight.save
+  redirect to("/flights")
+end
+
+post '/flights/:id/delete' do
+  Flight.delete(params[:id])
+  redirect to("/flights")
+end
